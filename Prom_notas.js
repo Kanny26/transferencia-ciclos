3bdd6269df5de19f5610ee320d68a26f04d7d185
stephany <stephanymoreno1826@gmail.com>
2025-10-15 15:42:39 -0500

    Se solicita el ingreso de 3 notas de 5 estudiantes y determinar su promedio

---
+++ Prom_notas.js
@@ -0,0 +1,23 @@
+// Ejercicio 4 – Promedio de notas
+// Crea un programa que: lea las notas de 5 estudiantes. Calcula el promedio de cada uno y
+// determina, mediante condicionales, si aprueba (nota ≥ 3.0) o reprueba. Usa ciclos para
+// ingresar los datos de manera ordenada.
+
+//ciclo para ingresar las notas de 5 estudiantes
+for (let i = 1; i <= 5; i++) {
+    alert("Estudiante "+ i);
+
+    //solicitud de notas
+    let nota1 = parseInt(prompt("Ingrese la primera nota:"));
+    let nota2 = parseInt(prompt("Ingrese la segunda nota:"));
+    let nota3 = parseInt(prompt("Ingrese la tercera nota:"));
+
+    //calculo del promedio
+    let promedio = (nota1 + nota2 + nota3) / 3;
+
+    if (promedio >= 3.0){
+        alert("El estudiante " + i + " ha aprodbado con un promedio de " + promedio.toFixed(2));
+    }else {
+        alert("El estudiante " + i + " ha reprobado con un promedio de " + promedio.toFixed(2));
+    }
+}
